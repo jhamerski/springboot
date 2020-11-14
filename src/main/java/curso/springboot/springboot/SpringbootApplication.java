0b42906e1a3426817d64c2af83ed393c5ba72c23
Jonas Hamerski <jonashamerski87@gmail.com>
2020-11-14 02:56:53 -0300

    Primeiro projeto com Spring boot

---
+++ src/main/java/curso/springboot/springboot/SpringbootApplication.java
@@ -0,0 +1,17 @@
+package curso.springboot.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EntityScan(basePackages = "curso.springboot.model")
+@ComponentScan(basePackages = "curso.*")
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+}
